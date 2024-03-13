d76d9a90c4ba3e1dc8a8dbb0a28bf2ba562bfda0
Mikemupararano <kudath@yahoo.co.uk>
2024-03-13 20:40:47 +0000

    Added App.jsx and App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
+import ContactPage from './components/Contact/Contact';
+import Projects from './components/Projects/Projects';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
-  return <h1>Hello Team!</h1>
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
