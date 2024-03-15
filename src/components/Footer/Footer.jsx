196341864de0d2788ec22a94be72f36507df6691
Mikemupararano <kudath@yahoo.co.uk>
2024-03-15 16:42:40 +0000

    Added templates to add Github links to footer for each group member.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Footer.css';
 // import GitHubIcon from '@mui/icons-material/GitHub';
+//Github link for each member of the group
 const Footer = () => {
   return (
     <div className="footer">
+    
       <div className="socialMedia">
         <a href="https://github.com/Mikemupararano/revision-app" target="_blank" rel="noopener noreferrer">
+          {/* <GitHubIcon /> */}
+        </a>
+         <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+          {/* <GitHubIcon /> */}
+        </a>
+         <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+          {/* <GitHubIcon /> */}
+        </a>
+         <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+          {/* <GitHubIcon /> */}
+        </a>
+         <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
           {/* <GitHubIcon /> */}
         </a>
         </div>
