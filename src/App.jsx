e45ddb4c9e31b7a87964beec1416381c3660930a
Andreea-Lita <litaandreeamalaiasi@gmail.com>
2024-03-18 17:04:31 +0000

    code snnippet JS introduced by user is executed and gives an output

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import CodeSection from './components/CodeSection/CodeSection'
-function App() {
+import React from 'react';
+import CodeSection from './components/CodeSection/CodeSection';
 
-  return <CodeSection/>
+// import ParentComponent from './components/CodeSection/CodeSection';
+
+function App() {
+  const someCode = '// This is a code snippet'; // Replace with actual code
+
+  return (
+    <div>
+      <CodeSection /> {/* Render CodeSection component */}
+     
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+// import CodeSection from './components/CodeSection/CodeSection'
+// function App() {
+
+//   return <CodeSection/>
+// }
+
+// export default App
